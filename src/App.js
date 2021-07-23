3b4eed33e38129219a9ee7f027bb116faaae4622
Sam Clark <clarksam19@gmail.com>
2021-07-23 11:15:41 -0500

    part 1.b

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-};
-
-const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>
-}
-
-const Content = (props) => {
-  const courses = props.courses;
-  return (
-    <div>
-      <Part name={courses.part1.name} exercises={courses.part1.exercises} />
-      <Part name={courses.part2.name} exercises={courses.part2.exercises} />
-      <Part name={courses.part3.name} exercises={courses.part3.exercises} />
-    </div>
-  )
-};
-
-const Total = (props) => {
-  const total = Object.values(props.courses).map(part => {
-    return part.exercises;
-  }).reduce((acc, num) => acc + num);
-
-  return <p>{total}</p>
-};
+import Course from "./components/Course";
 
 const App = () => {
-  const course = 'Half Stack applciation development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-
-  const courses = {
-    part1,
-    part2,
-    part3,
-  }
-
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+    
   return (
     <div>
-      <Header course={course} />
-      <Content courses={courses} />
-      <Total courses={courses} />
+      <h1>Web Development Curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
-  );
+  )
 }
 
 export default App;
